b2254c8a6b81e6515a6212464ccf4711f4573985
istravik <istravik@88ec2f54-9dd9-11de-a36b-d11e950a0b04>
2009-09-10 07:42:37 +0000

    PlayerShip

---
+++ SpaceInvaders/src/gamecore/PlayerShip.java
@@ -0,0 +1,17 @@
+
+package gamecore;
+
+public class PlayerShip {
+    private int position, speed;
+    private int modelHeight, modelWidth;
+    private int impactArea;
+    boolean hasActiveProjectile;
+
+    public PlayerShip() {
+        //konstruktor
+    }
+
+    public void fireBullet() {
+        
+    }
+}
