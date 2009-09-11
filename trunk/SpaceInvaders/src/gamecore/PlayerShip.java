7f673563d6f31e0b8be81917501c7e9bb5fc712e
istravik <istravik@88ec2f54-9dd9-11de-a36b-d11e950a0b04>
2009-09-11 12:31:46 +0000

    PlayerShip

--- trunk/SpaceInvaders/src/gamecore/PlayerShip.java
+++ trunk/SpaceInvaders/src/gamecore/PlayerShip.java
@@ -12,6 +12,58 @@ public class PlayerShip {
     }
 
     public void fireBullet() {
-        
+        //avfyra ett skott
+    }
+
+    public void moveShip() {
+        //flytta skeppet
+    }
+
+    public boolean isHasActiveProjectile() {
+        return hasActiveProjectile;
+    }
+
+    public int getImpactArea() {
+        return impactArea;
+    }
+
+    public int getModelHeight() {
+        return modelHeight;
+    }
+
+    public int getModelWidth() {
+        return modelWidth;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setHasActiveProjectile(boolean hasActiveProjectile) {
+        this.hasActiveProjectile = hasActiveProjectile;
+    }
+
+    public void setImpactArea(int impactArea) {
+        this.impactArea = impactArea;
+    }
+
+    public void setModelHeight(int modelHeight) {
+        this.modelHeight = modelHeight;
+    }
+
+    public void setModelWidth(int modelWidth) {
+        this.modelWidth = modelWidth;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
