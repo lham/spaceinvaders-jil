53bbc595cc63fc6bf46d034373f16db759264aa2
Linus Hammarlund <lham@users.noreply.github.com>
2009-09-22 21:51:13 +0000

    Updated methods in Coordinate and Area to a working state, removed bad methods in Bullet.

--- SpaceInvaders/src/gamecore/Bullet.java
+++ SpaceInvaders/src/gamecore/Bullet.java
@@ -16,52 +16,12 @@ public class Bullet extends ViewableObject{
         this.direction = direction;
     }
 
-    public void setImpactArea(int impactArea) {
-        this.impactArea = impactArea;
-    }
-
-    public void setModelHeight(int modelHeight) {
-        this.modelHeight = modelHeight;
-    }
-
-    public void setModelWidth(int modelWidth) {
-        this.modelWidth = modelWidth;
-    }
-
-    public void setPositionX(int positionX) {
-        this.positionX = positionX;
-    }
-
-    public void setPositionY(int positionY) {
-        this.positionY = positionY;
-    }
-
     public void setSpeed(int speed) {
         this.speed = speed;
     }
 
     public int getDirection() {
         return direction;
-    }
-
-    public int getImpactArea() {
-        return impactArea;
-    }
-
-    public int getModelHeight() {
-        return modelHeight;
-    }
-
-    public int getModelWidth() {
-        return modelWidth;
-    }
-
-    public int getPositionX() {
-        return positionX;
-    }
-
-    public int getPositionY() {
-        return positionY;
     }
 
     public int getSpeed() {
--- SpaceInvaders/src/gamecore/Coordinate.java
+++ SpaceInvaders/src/gamecore/Coordinate.java
@@ -1,9 +1,56 @@
 package gamecore;
 
 /**
- *
+ * Coordinate contains the x-value and y-value of a pixel on the screen.
  * @author Linus
  */
 public class Coordinate {
-    int x, y;
+    private int x, y;
+
+    /**
+     * Constructor, sets the values of the Coordinate.
+     * @param x The value x will be assigned.
+     * @param y The value y will be assigned.
+     */
+    public Coordinate (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the x-value of the Coordinate.
+     * @return x-value of the Coordinate
+     */
+    public int getX(){
+        return this.x;
+    }
+
+    /**
+     * Set the x-value of the Coordinate.
+     * @param x The value x will be assigned.
+     */
+    public void setX(int x){
+        this.x = x;
+    }
+
+    /**
+     * Returns the y-value of the Coordinate.
+     * @return y-value of the Coordinate
+     */
+    public int getY(){
+        return this.y;
+    }
+
+    /**
+     * Set the y-value of the Coordinate.
+     * @param y The value y will be assigned.
+     */
+    public void setY(int y){
+        this.y = y;
+    }
+
+    @Override
+    public String toString(){
+        return "x=" + this.x + "; y=" + this.y;
+    }
 }
