65c28d88c930c59c2b23073f72a8e0249df35253
linus.snail <linus.snail@88ec2f54-9dd9-11de-a36b-d11e950a0b04>
2009-09-24 10:14:07 +0000

    Area finished

--- trunk/SpaceInvaders/src/gamecore/Area.java
+++ trunk/SpaceInvaders/src/gamecore/Area.java
@@ -6,15 +6,20 @@ package gamecore;
  * @author Linus
  */
 public class Area {
-    Coordinate lowLeftCorner, topRightCorner;
+    private Coordinate lowLeftCorner, topRightCorner;
 
+    /**
+     * Constructor, sets the area from of two Coordinates.
+     * @param lowLeftCorner     The Coordinate for the lowLeftCorner.
+     * @param topRightCorner    the Coordinate for the topRightcorner.
+     */
     public Area (Coordinate lowLeftCorner, Coordinate topRightCorner){
-        if (lowLeftCorner.getX() <= topRightCorner.getX() && lowLeftCorner.getY() <= topRightCorner.getY()){
+        if (lowLeftCorner.getX() <= topRightCorner.getX() && lowLeftCorner.getY() >= topRightCorner.getY()){
             this.lowLeftCorner = lowLeftCorner;
             this.topRightCorner = topRightCorner;
         }
         else {
-            //felmeddelande eller exception...
+            //Kasta exception eller något käckt..
         }
     }
 
@@ -23,7 +28,7 @@ public class Area {
      * @return The lenght of the base of the area.
      */
     public int getLenght(){
-        return this.topRightCorner.getX() - this.lowLeftCorner.getX();
+        return this.topRightCorner.getX() - this.lowLeftCorner.getX() + 1;
     }
 
     /**
@@ -31,25 +36,168 @@ public class Area {
      * @return The height of the side of the area.
      */
     public int getHeight(){
-        return this.topRightCorner.getY() - this.lowLeftCorner.getY();
+        return this.lowLeftCorner.getY() - this.topRightCorner.getY() + 1;
     }
 
 
+    /**
+     * Returns the Coordinate of the lowLeftCorner.
+     * @return Coordinate of the lowLeftCorner.
+     */
     public Coordinate getLowLeftCorner(){
         return this.lowLeftCorner;
     }
+
+    /**
+     * Returns the Coordinate of the lowRightCorner.
+     * @return Coordinate of the lowRightCorner.
+     */
     public Coordinate getLowRightCorner(){
         return new Coordinate(this.topRightCorner.getX(), this.lowLeftCorner.getY());
     }
+
+    /**
+     * Returns the Coordinate of the topRightCorner.
+     * @return Coordinate of the topRightCorner.
+     */
     public Coordinate getTopRightCorner(){
         return this.topRightCorner;
     }
+
+    /**
+     * Returns the Coordinate of the topLeftCorner.
+     * @return Coordinate of the topLeftCorner.
+     */
     public Coordinate getTopLeftCorner(){
         return new Coordinate(this.lowLeftCorner.getX(), this.topRightCorner.getY());
     }
 
-    //Gör setmetoder för ovan + javadoc
+    /**
+     * Set the position of the lowLeftCorner.
+     * @param newX The new x-value.
+     * @param newY The new y-value.
+     * @return <code>true</code> if the corner was set successfully, otherwise
+     * it returns <code>false</code>
+     */
+    public boolean setLowLeftCorner(int newX, int newY){
+        if (newX > this.topRightCorner.getX() || newY < this.topRightCorner.getY()){
+            return false;
+        }
+        else {
+            this.lowLeftCorner.setX(newX);
+            this.lowLeftCorner.setY(newY);
+            return true;
+        }
+    }
 
+    /**
+     * Set the position of the lowLeftCorner.
+     * @param coord The Coordinate lowLeftCorner will be assigned to.
+     * @return <code>true</code> if the corner was set successfully, otherwise
+     * it returns <code>false</code>
+     */
+    public boolean setLowLeftCorner(Coordinate coord){
+        if (this.setLowLeftCorner(coord.getX(), coord.getY()))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Set the position of the topRightCorner.
+     * @param newX The new x-value.
+     * @param newY The new y-value.
+     * @return <code>true</code> if the corner was set successfully, otherwise
+     * it returns <code>false</code>
+     */
+    public boolean setTopRightCorner(int newX, int newY){
+        if (newX < this.lowLeftCorner.getX() || newY > this.lowLeftCorner.getY()){
+            return false;
+        }
+        else {
+            this.topRightCorner.setX(newX);
+            this.topRightCorner.setY(newY);
+            return true;
+        }
+    }
+
+    /**
+     * Set the position of the topRightCorner.
+     * @param coord The Coordinate topRightCorner will be assigned to.
+     * @return <code>true</code> if the corner was set successfully, otherwise
+     * it returns <code>false</code>
+     */
+    public boolean setTopRightCorner(Coordinate coord){
+        if (this.setTopRightCorner(coord.getX(), coord.getY()))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Moves the lowLeftCorner of the area x steps in x-direction and y steps in
+     * y-direction
+     * @param moveX How much the corner will move in x-direction.
+     * @param moveY How much the corner will move in y-direction.
+     * @return <code>true</code> if the corner was moved successfully, otherwise
+     * it returns <code>false</code>
+     */
+    public boolean moveLowLeftCorner(int moveX, int moveY){
+        if (this.lowLeftCorner.getX() + moveX > this.topRightCorner.getX()){
+            return false;
+        }
+        else if (this.lowLeftCorner.getY() + moveY < this.topRightCorner.getY()){
+            return false;
+        }
+        else {
+            this.lowLeftCorner.setX(this.lowLeftCorner.getX() + moveX);
+            this.lowLeftCorner.setY(this.lowLeftCorner.getY() + moveY);
+            return true;
+        }
+    }
+
+    /**
+     * Moves the topRightCorner of the area x steps in x-direction and y steps in
+     * y-direction
+     * @param moveX How much the corner will move in x-direction.
+     * @param moveY How much the corner will move in y-direction.
+     * @return <code>true</code> if the corner was moved successfully, otherwise
+     * it returns <code>false</code>
+     */
+    public boolean moveTopRightCorner(int moveX, int moveY){
+        if (this.topRightCorner.getX() + moveX < this.lowLeftCorner.getX()){
+            return false;
+        }
+        else if (this.topRightCorner.getY() + moveY > this.lowLeftCorner.getY()){
+            return false;
+        }
+        else {
+            this.topRightCorner.setX(this.topRightCorner.getX() + moveX);
+            this.topRightCorner.setY(this.topRightCorner.getY() + moveY);
+            return true;
+        }
+    }
+
+    /**
+     * Moves the Area x steps in x-direction and y steps in y-direction.
+     * @param moveX How much the area will move in x-direction.
+     * @param moveY How much the area will move in y-direction.
+     */
+    public void moveArea(int moveX, int moveY){
+        this.setArea(
+                    new Coordinate(this.lowLeftCorner.getX() + moveX, this.lowLeftCorner.getY() + moveY),
+                    new Coordinate(this.topRightCorner.getX() + moveX, this.topRightCorner.getY() + moveY));
+    }
+
+    private void setArea(Coordinate lowLeftCorner, Coordinate topRightCorner){
+        if (lowLeftCorner.getX() <= topRightCorner.getX() && lowLeftCorner.getY() >= topRightCorner.getY()){
+            this.lowLeftCorner = lowLeftCorner;
+            this.topRightCorner = topRightCorner;
+        }
+        else {
+            //Kasta exception eller något käckt..
+        }
+    }
     
     
     /**
@@ -62,7 +210,7 @@ public class Area {
         if (coord.getX() < area.getLowLeftCorner().getX() || coord.getX() > area.getTopRightCorner().getX()){
             return false;
         }
-        else if (coord.getY() < area.getLowLeftCorner().getY() || coord.getY() > area.getTopRightCorner().getY()){
+        else if (coord.getY() > area.getLowLeftCorner().getY() || coord.getY() < area.getTopRightCorner().getY()){
             return false;
         }
         else {
