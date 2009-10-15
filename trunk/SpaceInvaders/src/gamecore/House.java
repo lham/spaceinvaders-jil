a394e822825ba339bff19319ef4248b4fb76fc5f
JonG.Jakobsson <JonG.Jakobsson@88ec2f54-9dd9-11de-a36b-d11e950a0b04>
2009-10-15 07:33:17 +0000

    Profited in House

--- trunk/SpaceInvaders/src/gamecore/House.java
+++ trunk/SpaceInvaders/src/gamecore/House.java
@@ -10,6 +10,10 @@ public class House extends ViewableObject{
     private int health;
     //private Area houseSections[];
 
+    public House(Coordinate spawnCoord) {
+        super(new Area(spawnCoord, new Coordinate(spawnCoord.getX()+30, spawnCoord.getY()-40))); //will change later to height and width of sprite
+    }
+
     public void destroyPart(/*Area destroyedPixels*/){
 
     }
